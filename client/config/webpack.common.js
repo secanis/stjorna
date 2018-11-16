ad7d04f5a3b5c3a7b469e841d3227c12b2820b40
Matthias Baldi <matthias.baldi@secanis.ch>
2018-11-16 12:17:22 +0100

    fix: improve webpack config

--- client/config/webpack.common.js
+++ client/config/webpack.common.js
@@ -88,7 +88,19 @@ module.exports = {
       {} // a map of your routes
     ),
     new HtmlWebpackPlugin({
-      template: 'src/public/index.html'
+      title: 'STJ&Oacute;RNA - Comfortable Product Managment.',
+      meta: {
+        title: 'STJ&Oacute;RNA - Comfortable Product Managment.',
+        author: 'secanis.ch',
+        publisher: 'secanis.ch',
+        copyright: 'secanis.ch',
+        charset: 'UTF-8',
+        viewport: 'width=device-width, initial-scale=1',
+      },
+      favicon: 'src/public/assets/favicon.ico',
+      // baseHref: '/',
+      template: 'src/public/index.html',
+      // inject: 'head'
     }),
     new CopyWebpackPlugin([
       { from: 'src/public/assets/i18n/*.json', to: 'assets/i18n', flatten: true },
