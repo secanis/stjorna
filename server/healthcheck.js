b437afb59c495360b62b5062bc97b49df5c72650
Matthias Baldi <matthias.baldi@secanis.ch>
2018-12-04 16:10:24 +0100

    fix: fix healthcheck

--- server/healthcheck.js
+++ server/healthcheck.js
@@ -3,12 +3,12 @@ const http = require("http");
 
 const options = {
     host: "localhost",
-    port: process.env.STJORNA_SERVER_PORT,
+    port: process.env.STJORNA_SERVER_PORT || 3000,
     timeout: 2000
 };
 
 let request = http.request(options, (res) => {
-    console.info(`STATUS: ${res.statusCode}`);
+    console.info(`STATUS: ${res.statusMessage}`);
     if (res.statusCode == 200) {
         process.exit(0);
     } else {
@@ -17,6 +17,8 @@ let request = http.request(options, (res) => {
 });
 
 request.on('error', (err) => {
+    console.log('STJORNA_SERVER_PORT', process.env.STJORNA_SERVER_PORT);
+    console.error(err);
     console.error('ERROR: unexpected error, please check the logs');
     process.exit(1);
 });
