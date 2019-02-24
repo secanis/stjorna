16519102ab6ee9d462a9df9f8e77cd20e9d00fc4
Matthias Baldi <matthias.baldi@secanis.ch>
2019-02-24 19:01:50 +0100

    fix: improvments for better image quality

--- server/lib/env_default.js
+++ server/lib/env_default.js
@@ -2,7 +2,7 @@ module.exports = {
     initialize: () => {
         // application startup parameters
         process.env['STJORNA_SERVER_PORT']                  = process.env.STJORNA_SERVER_PORT               || 3000;
-        process.env['STJORNA_SERVER_MAX_UPLOAD']            = process.env.STJORNA_SERVER_MAX_UPLOAD         || '5mb';
+        process.env['STJORNA_SERVER_MAX_UPLOAD']            = process.env.STJORNA_SERVER_MAX_UPLOAD         || '8mb';
         process.env['STJORNA_LOGLEVEL']                     = process.env.STJORNA_LOGLEVEL                  || 'info';
         process.env['STJORNA_REQUEST_LOG']                  = process.env.STJORNA_REQUEST_LOG               || '';
         process.env['STJORNA_CRON_CLEANUP_INTERVAL']        = process.env.STJORNA_CRON_CLEANUP_INTERVAL     || '00 3 * * *';
@@ -17,7 +17,7 @@ module.exports = {
         // configuration default values
         process.env['STJORNACONFIG_PASSWORD_SECRECT']       = process.env.STJORNACONFIG_PASSWORD_SECRECT    || '';
         process.env['STJORNACONFIG_ALLOW_REMOTE_ACCESS']    = process.env.STJORNACONFIG_ALLOW_REMOTE_ACCESS || false;
-        process.env['STJORNACONFIG_IMAGE_DIMENSION']        = process.env.STJORNACONFIG_IMAGE_DIMENSION     || 350;
+        process.env['STJORNACONFIG_IMAGE_DIMENSION']        = process.env.STJORNACONFIG_IMAGE_DIMENSION     || 700;
         process.env['STJORNACONFIG_IMAGE_QUALITY']          = process.env.STJORNACONFIG_IMAGE_QUALITY       || 70;
         process.env['STJORNACONFIG_INSTALLED']              = process.env.STJORNACONFIG_INSTALLED           || false;
     },
