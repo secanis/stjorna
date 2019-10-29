920be9058498a9710eebec63c45c86520edd4d9e
Matthias Baldi <matthias.baldi@secanis.ch>
2019-10-30 00:08:19 +0100

    fix: update documentation for 2.4.x releases

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,15 +1,20 @@
 define({
   "name": "STJÓRNA",
-  "version": "1.0.0",
+  "version": "2.4.1",
   "description": "STJÓRNA was created to have an easy product management with the possibility to access the categories and products over an simple just readable REST API.\nExample Url:\nhttps://STJORNA_SERVER_URL/api/v1/categories?apikey=APIKEY&userid=USERID_TOKEN",
   "title": "STJÓRNA REST API Documentation",
+  "url": "{yourStjornaUrl}",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-06-29T12:08:51.810Z",
+    "time": "2019-10-29T21:45:00.424Z",
     "url": "http://apidocjs.com",
-    "version": "0.17.6"
+    "version": "0.17.7"
   }
 });
